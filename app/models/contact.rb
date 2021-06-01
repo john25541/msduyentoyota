ff535445ad51c8396f15fd7a20ce79856d8da9c1
John Nguyen <nguyenvanloc1247@gmail.com>
2021-06-01 21:19:23 +0700

    custom contacts error message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates :name, :phone_number, :message, presence: true
-    validates :phone_number, numericality: true,
-                            length: {minimum: 10, maximum: 15}
+  validates :name, :phone_number, :message, presence: true
+  validates :phone_number, numericality: true,
+                          length: {minimum: 10, maximum: 15}
 end
