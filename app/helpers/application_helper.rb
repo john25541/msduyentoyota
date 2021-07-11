c96be1f5de9ab4c041d36645d15369641654e1fb
John Nguyen <nguyenvanloc1247@gmail.com>
2021-07-11 23:47:35 +0700

    add body_class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
+    
   def is_active?(action)
     params[:controller] == action ? "active" : nil        
+  end
+  def body_class
+    "#{controller_path.gsub('/', '_')} #{action_name} #{controller_name}-#{action_name} #{mobile_class}"
+  end
+  def mobile_class
+    is_mobile? ? 'mobile' : ''
+  end
+  def is_mobile?
+    request.user_agent =~ /Mobile|webOS/
   end
 end
