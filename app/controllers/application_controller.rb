255a70dae6bd077184675510e27be8678154001f
John Nguyen <nguyenvanloc1247@gmai.com>
2021-10-02 20:02:42 +0700

    Update template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  $car_models = CarModel.all
+  before_action :set_car_model
+
+  protected
+
+  def set_car_model
+    @car_models = CarModel.all
+  end
 
   def after_sign_in_path_for(resource)
     admins_path
