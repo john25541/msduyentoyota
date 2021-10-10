1390e4c4ecf92660a7ec9326ab3fae7b98e83898
john25541 <nguyenvanloc1247@gmail.com>
2021-10-10 20:28:56 +0700

    delete thumbnail_image car models

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -4,7 +4,7 @@ class CarModelsController < ApplicationController
     @car_model = CarModel.includes(:products).find_by_slug(params[:slug])
   end
 
-  private ####
+  private
 
   def car_model_params
     params.require(:car_model).permit(:name, :image)
--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,6 +1,5 @@
 class CarModel < ApplicationRecord
   has_one_attached :image
-  has_one_attached :thumbnail_image
   has_many :products
 
 end
