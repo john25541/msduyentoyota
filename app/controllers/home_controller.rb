93ec4d8d5b1923b69808a1e9a6c4bfdc9bf9e533
john25541 <nguyenvanloc1247@gmail.com>
2021-10-17 10:28:07 +0700

    Update products on feature car

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @products = Product.limit(6)
-    @all_products = Product.all.limit(12)
+    @all_products = Product.all.limit(9)
   end
 end
