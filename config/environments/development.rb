eb88db4066b2db2acc1a68998d4321f2cc6667d5
john25541 <nguyenvanloc1247@gmail.com>
2021-10-27 10:49:50 +0700

    Change active_storage on develop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
 
   config.action_mailer.perform_caching = false
 
