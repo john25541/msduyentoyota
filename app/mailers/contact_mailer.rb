e87f4cbb147412fa00b576a105933286418f4f20
john25541 <nguyenvanloc1247@gmail.com>
2021-11-29 22:39:12 +0700

    Update info developer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ApplicationMailer
 
 	default :from => "\"Ms.Duyen Toyota Gò Vấp\" <no-reply@toyotagovap.vn>",
-          reply_to: -> { "minhduyen0608@gmail.com" }
+          reply_to: -> { "nguyenvanloc1247@gmail.com" }
   def notification
     @contact = params[:contact]
-    mail(to: 'minhduyen0608@gmail.com', subject: 'Thông tin Khách hàng cần liên hệ',
+    mail(to: 'nguyenvanloc1247@gmail.com', subject: 'Thông tin Khách hàng cần liên hệ',
       cc: 'nguyenvanloc1247@gmail.com')
   end
 end
